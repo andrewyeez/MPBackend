02f4cbeda234e29b0fcb54ce6a5b9f36200bb940
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-13 22:43:38 -0700

    updated app.js

--- app/app.js
+++ app/app.js
@@ -2,11 +2,9 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('change Nuts!')
+  res.send('Hello!')
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-
-// docker run -v /Users/AndrewPro/Desktop/MealPlanner/backend:/src -d -p 3000:3000 backend
