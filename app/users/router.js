d2508241cd39380036645830a5e330b5f49cfab4
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-16 09:45:09 -0700

    space -

--- app/users/router.js
+++ app/users/router.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var router = express.Router()
 
-
 // adding our controller files
 var all = require('./controllers/all')
 var create = require('./controllers/create')
