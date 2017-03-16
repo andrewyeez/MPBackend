9443065ccf6480201b6b2a54396e3cdfa7c87212
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-16 09:43:54 -0700

    defining the users controller files

---
+++ app/users/controllers/update.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var router = express.Router()
+
+router.put('/', function (req,res) {
+  res.send('PUT request needs to be implemented')
+})
+
+module.exports = router
