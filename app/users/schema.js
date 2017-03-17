226c0835c2a8ad5105a5602c96154738d4492751
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-17 07:20:02 -0700

    updated the users schema to have the relationship with meals

--- app/users/schema.js
+++ app/users/schema.js
@@ -1,24 +1,22 @@
+/**
+ *
+ * Relationship
+ *
+ * A Meal belongs to a User
+ * A User has many Meals
+ *
+ */
 var mongoose = require('mongoose')
-var autoIncrement = require('mongoose-auto-increment')
-
-// Connect to MongoDB and create/use database called todoAppTest
-var con = mongoose.connect('mongodb://192.168.99.100:27017/mpbackend');
-autoIncrement.initialize(con);
 
 // define what a USER is
 var userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: false },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  meals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meal' }]
 },
 {
   timestamps: true
-})
-
-// Auto Increment _id starting at 1
-userSchema.plugin(autoIncrement.plugin, {
-  model: 'User',
-  startAt: 1
 })
 
 var User = mongoose.model('User', userSchema)
