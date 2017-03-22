d00c1b5a75ac1c579ea0a9e3add3385ed9926477
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-21 20:47:37 -0700

    update the router for days

--- app/days/router.js
+++ app/days/router.js
@@ -3,14 +3,16 @@ var express = require('express')
 var router = express.Router()
 
 // adding our controller files
-var all = require('./controllers/all')
-var create = require('./controllers/create')
+var all = require('./controllers/create')
 var update = require('./controllers/update')
-var find_by_id = require('./controllers/find_by_id')
+var find_by_day = require('./controllers/find_by_day')
+var find_by_week = require('./controllers/find_by_week')
+var find_by_month = require('./controllers/find_by_month')
 
 router.use('/', all)
-router.use('/', create)
-router.use('/', all)
-router.use('/', find_by_id)
+router.use('/', update)
+router.use('/', find_by_day)
+router.use('/', find_by_week)
+router.use('/', find_by_month)
 
 module.exports = router
