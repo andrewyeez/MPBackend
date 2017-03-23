719be36ac3a781d916d191f19524f78e6a85772d
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-23 10:49:28 -0700

    updated schema to require the db connector file

--- app/users/schema.js
+++ app/users/schema.js
@@ -6,20 +6,17 @@
  * A User has many Meals
  *
  */
-var mongoose = require('mongoose')
 
-// define what a USER is
+var mongoose = require('mongoose')
+var connection = require('../../db')
+
 var userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: false },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  meals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meal' }]
+  password: { type: String, required: true, unique:false }
 },
 {
   timestamps: true
 })
 
-var User = mongoose.model('User', userSchema)
-
-// export our USER so it can be used in our Node application
-module.exports = User
+module.exports = connection.model('User', userSchema)
