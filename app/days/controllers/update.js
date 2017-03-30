a7c0bc9148c62c043ad5079ae5f8090e23bd1289
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-29 21:20:17 -0700

    derp. really need to end all my typos

--- app/days/controllers/update.js
+++ app/days/controllers/update.js
@@ -2,11 +2,11 @@ var express = require('express')
 var Day = require('../model')
 var router = express.Router()
 
-Day.put('/:id', function (req,res) {
+router.put('/:id', function (req,res) {
   Day.findByIdAndUpdate(req.param('id'), req.param, function (err) {
     if (err) return res.send(err)
     res.send({"message":"Success on updating!"})
   })
 })
 
-mondule.exports = router
+module.exports = router
