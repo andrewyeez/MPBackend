f37ef6e9c99986a02be61ce92e952d94ccae1bee
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-29 21:31:05 -0700

    rename all_by_user to days_by_user

--- app/days/router.js
+++ app/days/router.js
@@ -3,12 +3,12 @@ var router = express.Router()
 
 var create = require('./controllers/create')
 var update = require('./controllers/update')
-var all_by_user = require('./controllers/all_by_user')
+var days_by_user = require('./controllers/days_by_user')
 var day_by_user = require('./controllers/day_by_user')
 
 router.use('/', create)
 router.use('/', update)
-router.use('/', all_by_user)
+router.use('/', days_by_user)
 router.use('/', day_by_user)
 
 module.exports = router
