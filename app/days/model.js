78ba31becf431e654768f209ac6f818c0bb90472
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-03-29 20:03:50 -0700

    implement the Model for Days

--- app/days/model.js
+++ app/days/model.js
@@ -1,4 +1,22 @@
 /**
  *
+ *  Days
  *
  */
+
+ var mongoose = require('mongoose')
+ var connection = require('../../db')
+
+ var daySchema = new mongoose.Schema({
+   date: {type: Date, required: true},
+   morning: {type: Array},
+   snack1: {type: Array},
+   lunch: {type: Array}
+   snack2: {type: Array},
+   dinner: {type: Array}
+ },
+ {
+   timestamps: true
+ })
+
+ module.exports = connection.model('Day', daySchema)
